1c05d288a335f504914561d2b96f5b4b8edba3d0
Tony Gorez <gorez.tony@gmail.com>
2023-05-31 18:16:15 +0200

    feat: embbed wait for server logic into the test framework

--- test.cpp
+++ test.cpp
@@ -10,17 +10,6 @@ int main() {
   CURL *curl;
   CURLcode res;
 
-  int count = 0;
-
-  while (!std::ifstream("server.pid")) {
-    if (count > 3) {
-      std::cout << "Server failed to start" << std::endl;
-      return 1;
-    }
-    count++;
-    std::this_thread::sleep_for(std::chrono::seconds(1));
-  }
-
   curl_global_init(CURL_GLOBAL_DEFAULT);
 
   curl = curl_easy_init();
